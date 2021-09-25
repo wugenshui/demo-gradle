1c26ca56815fd7ec0bcf6990668af0a32410fcaa
chenbo <963838470@qq.com>
2021-09-25 20:37:37 +0800

    多模块项目

---
+++ common/src/main/java/com/github/wugenshui/common/Util.java
@@ -0,0 +1,14 @@
+package com.github.wugenshui.common;
+
+/**
+ * @author : chenbo
+ * @date : 2021-09-25
+ */
+public class Util {
+    /**
+     * 加法
+     */
+    public int add(int numA, int numB) {
+        return numA + numB;
+    }
+}
--- demo/src/main/java/com/github/wugenshui/Demo.java
+++ demo/src/main/java/com/github/wugenshui/Demo.java
@@ -1,13 +1,11 @@
 package com.github.wugenshui;
 
 /**
- * 测试 gradle
- *
  * @author : chenbo
  * @date : 2021-09-25
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println("demo");
     }
 }
