d53270efed1b1f3e20f3c86ff1dc9951daea0072
chenbo <963838470@qq.com>
2021-09-25 20:07:02 +0800

    简单的demo

---
+++ src/main/java/com/github/wugenshui/Demo.java
@@ -0,0 +1,13 @@
+package com.github.wugenshui;
+
+/**
+ * 测试 gradle
+ *
+ * @author : chenbo
+ * @date : 2021-09-25
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
